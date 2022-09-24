cd7faef58ca25837e28a1c0dc1453808bf1c8282
squrpe <83641173+squrpe@users.noreply.github.com>
2022-09-24 22:51:37 +0930

    After Tutor Changes

--- script.js
+++ script.js
@@ -7,57 +7,68 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var number = "0123456789";
+  var symbol = "~!@#$%^&*()_+{}:?><;.,";
+
+  var usableCharacters = " ";
+  var password = " ";
   
-  var lengthChoice = window.prompt("How long do you want your password?");
+  var lengthChoice = 0;
+  lengthChoice = window.prompt("How long do you want your password? (Between 8 and 128)");
+
+  if (lengthChoice >= 8 && lengthChoice <= 128) {
+    console.log(lengthChoice);
+    } else {
+    alert("Please be sure your response is a number between 8 and 128.")
+    return ' ';
+  }
+
   var lowerChoice = window.confirm("Do you want to include Lowercase Letters?");
+  if (lowerChoice) {
+    usableCharacters += lower;
+    console.log(usableCharacters);
+  }
+
   var upperChoice = window.confirm("Do you want to include Uppercase Letters?");
+  if (upperChoice) {
+    usableCharacters += upper;
+    console.log(usableCharacters);
+  }
+
   var numChoice = window.confirm("Do you want to include Numbers?");
+  if (numChoice) {
+    usableCharacters += number;
+    console.log(usableCharacters);
+  }
+
   var symChoice = window.confirm("Do you want to include Special Symbols?");
-
-  var generatePassword = " ";
-
-  var countChoices = lowerChoice + upperChoice + numChoice + symChoice;
-
-  var choiceArr = [{lowerChoice}, {upperChoice}, {numChoice}, {symChoice}].filter(item => Object.values(item)[0]);
-  console.log("Arr: ", choiceArr);
-
-  if(countChoices === 0) {
-    return " ";
+  if (symChoice) {
+    usableCharacters += symbol;
+    console.log(usableCharacters);
   }
 
-  for (var i = 0; i < lengthChoice; i+=countChoices) {
-    choiceArr.forEach(type => { })
+  if (!lowerChoice && !upperChoice && !numChoice && !symChoice){
+    alert("You must include at least one set of characters.")
+    return ' ';
+    }
 
-  }
+  for (var i = 0; i < lengthChoice; i++) {
 
+    password += usableCharacters[Math.floor(Math.random() * usableCharacters.length)];
+
+  };
   
-  return "placeholder";
+  return password;
+
 }
-
-//function trueValues(array) {
-
-  //console.log("Arr: ", array);
-
-  //var trueChoices;
-
-  //for (const key in array) {
-    //if (array.(key)) {
-     //console.log(`${key}: ${array[key]}`);
-    //}
-  //}
-
-
-  //for (var i = 0; i < array.length; i++) {
-    //if (array[i] === true)
-
-  //}
-
-  //return trueChoices;
-
 
